fc76fa6cba337fb3a433a9112be32e1b38f314c3
wonder <wonderdeveloper@gmail.com>
2020-05-18 16:38:52 +0300

    commit 1, make some base code

---
+++ word_generator.py
@@ -0,0 +1,12 @@
+from random import choice
+
+
+def get_file_data() -> list:
+    with open('word_rus.txt', 'rt', encoding='utf-8') as file:
+        return file.readlines()
+
+
+def get_random_word() -> str:
+    data = get_file_data()
+    return choice(data)
+
